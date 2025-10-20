af8a98e13b08abcdea25560895d6d4f6f0bb0d40
langel <knoxburry@gmail.com>
2025-10-19 22:21:19 -0400

    new_name collision abolished

--- backup_rom.sh
+++ backup_rom.sh
@@ -3,7 +3,7 @@
 count=$(ls -1q exports/ | wc -l)
 padding_length=4
 padded_count=$(printf "%0${padding_length}d" "$count")
-new_name="new_name__$padded_count.nes"	
+backup="new_name__$padded_count.nes"	
 	
-echo $new_name 
-cp new_name.nes exports/$new_name
+echo $backup
+cp new_name.nes exports/$backup
