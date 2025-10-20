42485fe14e152e097a491809c4669ca60aaa2d24
langel <knoxburry@gmail.com>
2025-10-20 18:29:00 -0400

    added label convertor

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-onchange -v -p 250 './*.asm' './src/*.asm' './objs/*.asm' -- sh -c 'echo compiling && dasm new_name.asm -Isrc -Iaudio -Ivisual -onew_name.nes -f3 -v2 -llisting.txt && echo launching && cmd.exe /C start new_name.nes'
+onchange -v -p 250 './*.asm' './src/*.asm' './objs/*.asm' -- sh -c 'echo compiling && dasm new_name.asm -Isrc -Iaudio -Ivisual -orom.nes -f3 -v2 -llisting.txt && echo launching && cmd.exe /C start rom.nes'
 # dasm -sromsym.txt will export symbol file
